c55cfa0a1dccfec4ce017d7832d534777c352c75
Joakim Tysseng <joakim.tysseng@entur.org>
2021-04-12 20:57:41 +0200

    Added midi in and out, just need to connect it to pot

--- src/midibus.ts
+++ src/midibus.ts
@@ -1,5 +1,6 @@
 import { MidiConfig } from './midiConstants'
 
+type MIDIMessageEvent = WebMidi.MIDIMessageEvent
 type MIDIInput = WebMidi.MIDIInput
 type MIDIOutput = WebMidi.MIDIOutput
 type MIDIAccess = WebMidi.MIDIAccess
@@ -13,90 +14,103 @@ type Subscriber = {
 const midiConfig = {
     inputIds: ['-961056816'],
     outputIds: ['-259958146'],
+    channel: 0,
 }
 
-let midiOut: MIDIOutput | undefined;
-let midiIn:  MIDIInput | undefined;
+let ccForChannel = 0b10110000 + midiConfig.channel
 
-const loopback = true;
-let idPool = 0;
-const subscribers: {[key: number]: Subscriber[]} = {}
+let midiOut: MIDIOutput | undefined
+let midiIn: MIDIInput | undefined
+
+const loopback = true
+let idPool = 0
+const subscribers: { [key: number]: Subscriber[] } = {}
 
 export const subscribe = (callback: (value: number) => void, { cc, values }: MidiConfig) => {
-    const id = idPool++;
-    subscribers[cc] = [...(subscribers[cc] || []), {id, values, callback}];
-    return id;
+    const id = idPool++
+    subscribers[cc] = [...(subscribers[cc] || []), { id, values, callback }]
+    return id
 }
 
 export const unsubscribe = (cc: number, id: number) => {
-    const subscribersForCC = subscribers[cc];
-    const index = subscribersForCC.map(sub => sub.id).indexOf(id);
-    if(index > -1){
-        subscribers[cc] = [...subscribersForCC.slice(0, index), ...subscribersForCC.slice(index+1)];
+    const subscribersForCC = subscribers[cc]
+    const index = subscribersForCC.map(sub => sub.id).indexOf(id)
+    if (index > -1) {
+        subscribers[cc] = [...subscribersForCC.slice(0, index), ...subscribersForCC.slice(index + 1)]
     }
 }
 
 const publishCC = (cc: number, value: number) => {
-    if(subscribers[cc]) {
+    if (subscribers[cc]) {
         subscribers[cc].forEach((subscriber) => {
-            if(!subscriber.values || subscriber.values.includes(value)){
-                subscriber.callback(value);
+            if (!subscriber.values || subscriber.values.includes(value)) {
+                subscriber.callback(value)
             }
         })
     }
 }
 
 export const sendCC = (cc: number, value: number) => {
-    console.log(`Sending ${cc} with value ${value}`);
-    if(loopback) {
-        publishCC(cc, value);
+    console.log(`Sending ${cc} with value ${value}`)
+    if (loopback) {
+        publishCC(cc, value)
     }
-    // TODO: Send with midi
+    if (midiOut) {
+        console.log('sending midi')
+        midiOut.send([ccForChannel, cc, value])
+    }
 }
 
-export const receiveCC = (cc: number, value: number) => {
-    // publish event
-    publishCC(cc, value);
+export const receiveMidiMessage = (midiEvent: MIDIMessageEvent) => {
+    const midiData = midiEvent.data
+    if (midiData[0] === ccForChannel) {
+        publishCC(midiData[1], midiData[2])
+    }
 }
 
-const updateSelectedMidi = (midiAccess: MIDIAccess) => {
-    console.log('UPDATING MIDI CONFIG');
+const updateSelectedMidi = async (midiAccess: MIDIAccess) => {
+    console.log('UPDATING MIDI CONFIG')
 
-    const foundInputId = midiConfig.inputIds.find(id => midiAccess.inputs.has(id));
-    const foundOutputId = midiConfig.outputIds.find(id => midiAccess.outputs.has(id));
+    const foundInputId = midiConfig.inputIds.find(id => midiAccess.inputs.has(id))
+    const foundOutputId = midiConfig.outputIds.find(id => midiAccess.outputs.has(id))
 
-    if(foundInputId){
-        midiIn = midiAccess.inputs.get(foundInputId);
-        console.log('Selected midi input', midiIn?.name);
+    if (foundInputId) {
+        midiIn = midiAccess.inputs.get(foundInputId)
+        if (midiIn) {
+            midiIn.onmidimessage = receiveMidiMessage
+        }
+        console.log('Selected midi input', midiIn?.name)
     } else {
-        midiIn = undefined;
-        console.log('Desired midi input not found');
+        midiIn = undefined
+        console.log('Desired midi input not found')
     }
 
-    if(foundOutputId){
-        midiOut = midiAccess.outputs.get(foundOutputId);
-        console.log('Selected midi output', midiOut?.name);
+    if (foundOutputId) {
+        midiOut = midiAccess.outputs.get(foundOutputId)
+        console.log('Selected midi output', midiOut?.name)
     } else {
-        midiOut = undefined;
-        console.log('Desired midi output not found');
+        midiOut = undefined
+        console.log('Desired midi output not found')
     }
 
 }
 
 const onMIDISuccess = (midiAccess: MIDIAccess) => {
 
-    updateSelectedMidi(midiAccess);
+    updateSelectedMidi(midiAccess)
 
-    midiAccess.onstatechange = (connectionEvent) => {
-        console.log(`Midi port ${connectionEvent.port.name} state changed to ${connectionEvent.port.state}, updating connections`);
-        updateSelectedMidi(midiAccess);
+    midiAccess.onstatechange = async (connectionEvent) => {
+        console.log(`Midi port ${connectionEvent.port.name} state changed to ${connectionEvent.port.state}, updating connections`)
+        await updateSelectedMidi(midiAccess)
     }
 }
 
 const onMIDIFailure = () => {
-    console.log('Could not access your MIDI devices.');
+    console.log('Could not access your MIDI devices.')
 }
 
-if(navigator.requestMIDIAccess) navigator.requestMIDIAccess({sysex: true}).then(onMIDISuccess, onMIDIFailure);
+if (navigator.requestMIDIAccess) {
+    navigator.requestMIDIAccess({ sysex: true }).then(onMIDISuccess, onMIDIFailure)
+}
 
 //TODO: Close midi connection on app close!
