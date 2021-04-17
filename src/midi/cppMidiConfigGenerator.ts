d2dfe696b8d5685484443d3ed7f7f46dc0d38c3b
Joakim Tysseng <joakim.tysseng@entur.org>
2021-04-17 22:47:35 +0200

    Slight renaming

--- src/midi/cppMidiConfigGenerator.ts
+++ src/midi/cppMidiConfigGenerator.ts
@@ -3,8 +3,8 @@ import midiControllers from './midiControllers'
 const generateCppFiles = () => {
     const buttonEnum: string[] = []
     const buttonCC: number[] = []
-    const buttonOffset: number[] = []
-    const buttonValuesCount: number[] = []
+    const buttonFirstValue: number[] = []
+    const buttonNumberOfValues: number[] = []
 
     const potEnum: string[] = []
     const potCC: number[] = []
@@ -15,8 +15,8 @@ const generateCppFiles = () => {
                 if(controller.type === 'button'){
                     buttonEnum.push(`BUTTON_${controllerGroupKey}_${controllerKey}`)
                     buttonCC.push(controller.cc)
-                    buttonOffset.push(controller.values ? controller.values[0] : 0)
-                    buttonValuesCount.push(controller.values?.length || 0)
+                    buttonFirstValue.push(controller.values ? controller.values[0] : 0)
+                    buttonNumberOfValues.push(controller.values?.length || 0)
                 } else if(controller.type === 'pot'){
                     potEnum.push(`POT_${controllerGroupKey}_${controllerKey}`)
                     potCC.push(controller.cc)
@@ -28,16 +28,16 @@ const generateCppFiles = () => {
 
     const buttonEnumFileContents = `enum Button: char {\n  ${buttonEnum.join(',\n  ')}\n};`;
     const buttonCCFileContents = `const char buttonCC[${buttonCC.length}] = {\n  ${buttonCC.join(',\n  ')}\n};`;
-    const buttonOffsetFileContents = `const char buttonOffset[${buttonOffset.length}] = {\n  ${buttonOffset.join(',\n  ')}\n};`;
-    const buttonValuesCountFileContents = `const char buttonValuesCount[${buttonOffset.length}] = {\n  ${buttonValuesCount.join(',\n  ')}\n};`;
+    const buttonFirstValueFileContents = `// First value in the button values array.\n// Values are sequential\nconst char buttonFirstValue[${buttonFirstValue.length}] = {\n  ${buttonFirstValue.join(',\n  ')}\n};`;
+    const buttonNumberOfValuesFileContents = `// Number of values for button\nconst char buttonNumberOfValues[${buttonNumberOfValues.length}] = {\n  ${buttonNumberOfValues.join(',\n  ')}\n};`;
 
     const potEnumFileContents = `enum Pot: char {\n  ${potEnum.join(',\n  ')}\n};`;
     const potCCFileContents = `const char potCC[${potCC.length}] = {\n  ${potCC.join(',\n  ')}\n};`;
 
     console.log('midiButtons.h', buttonEnumFileContents)
     console.log('midiButtonsCC.h', buttonCCFileContents)
-    console.log('midiButtonsOffset.h', buttonOffsetFileContents)
-    console.log('midiButtonsValuesCount.h', buttonValuesCountFileContents)
+    console.log('midiButtonsFirstValue.h', buttonFirstValueFileContents)
+    console.log('midiButtonsNumberOfValues.h', buttonNumberOfValuesFileContents)
     console.log('midiPots.h', potEnumFileContents)
     console.log('midiPotsCC.h', potCCFileContents)
 }
