d33f3b2eef4721eca6365e7664ef9e356e6b09f9
Joakim Tysseng <joakim.tysseng@entur.org>
2021-07-31 21:56:53 +0200

    Better handling of amount

--- src/synthcore/modules/mods/modsReducer.ts
+++ src/synthcore/modules/mods/modsReducer.ts
@@ -1,6 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { RootState } from '../../store'
-import { digitalModSources, modTarget } from '../../../midi/controllers'
 
 type ModsState = {
     gui: {
@@ -12,23 +11,6 @@ type ModsState = {
     modValues: number[][];
 }
 
-// TODO: This just adds 256 targets independently of how many there actually are.
-const getModValueTargets = () => {
-    const targets = []
-    for(let i=0; i<256; i++) {
-        targets.push(0)
-    }
-    return targets
-}
-
-const getModValuesArray = () => {
-    const sources = []
-    for(let i=0; i<256; i++) {
-        sources.push(getModValueTargets())
-    }
-    return sources
-}
-
 export const initialState: ModsState = {
     gui: {
         source: 0,
@@ -36,7 +18,7 @@ export const initialState: ModsState = {
         targetFunc: 0,
         targetParam: 0,
     },
-    modValues: getModValuesArray(),
+    modValues: [],
 }
 
 type GuiSourcePayload = {
@@ -78,6 +60,9 @@ export const modsSlice = createSlice({
             state.gui.targetParam = payload.guiTargetParam
         },
         setModValue: (state, { payload }: PayloadAction<ModValuePayload>) => {
+            if(!state.modValues[payload.sourceId]) {
+                state.modValues[payload.sourceId] = []
+            }
             state.modValues[payload.sourceId][payload.targetId] = payload.modValue
         },
 
@@ -96,6 +81,8 @@ export const selectGuiSource = (state: RootState) => state.mods.gui.source
 export const selectGuiTargetGroup = (state: RootState) => state.mods.gui.targetGroup
 export const selectGuiTargetFunc = (state: RootState) => state.mods.gui.targetFunc
 export const selectGuiTargetParam = (state: RootState) => state.mods.gui.targetParam
-export const selectModValue = (sourceId: number, targetId: number) => (state: RootState) =>  state.mods.modValues[sourceId][targetId]
+export const selectModValue = (sourceId: number, targetId: number) => (state: RootState) => {
+    return state.mods.modValues?.[sourceId]?.[targetId] || 0
+}
 
 export default modsSlice.reducer
