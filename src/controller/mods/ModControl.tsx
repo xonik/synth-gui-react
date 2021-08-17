197b0f757c80221af6583641c20e629b9d1ee665
Joakim Tysseng <joakim.tysseng@entur.org>
2021-08-17 21:48:01 +0200

    Added group name to corner.

--- src/controller/mods/ModControl.tsx
+++ src/controller/mods/ModControl.tsx
@@ -4,15 +4,21 @@ import TargetLabels from './TargetLabels'
 import AmountsTable from './AmountsTable'
 import ScrollSync from '../utils/scrollsync/ScrollSync'
 import ScrollSyncNode from '../utils/scrollsync/ScrollSyncNode'
+import { modTarget } from '../../synthcore/modules/mods/utils'
+import { useAppSelector } from '../../synthcore/hooks'
+import { selectGuiTargetGroup } from '../../synthcore/modules/mods/modsReducer'
 import './ModControl.scss'
 
 const ModControl = () => {
+    const targetGroupId = useAppSelector(selectGuiTargetGroup)
     return (
         <div className="mod-ctrl">
             <ScrollSync>
                 <>
                     <div className="mod-ctrl__header">
-                        <div className="mod-ctrl__header__corner"/>
+                        <div className="mod-ctrl__header__corner">
+                            {modTarget.groupLabels[targetGroupId]}
+                        </div>
                         <ScrollSyncNode lockAxis="Y">
                             <div className="mod-ctrl__header__targets-container">
                                 <TargetLabels/>
