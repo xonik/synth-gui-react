3d3ac13928b3b9798e1885795d0c059e588f66dd
Joakim Tysseng <joakim.tysseng@bekk.no>
2023-10-20 08:49:58 +0200

    Working midi RPC!

--- utils/midiRPC/generateMidiRPCDeserializer.ts
+++ utils/midiRPC/generateMidiRPCDeserializer.ts
@@ -5,14 +5,16 @@ export function generateMidiRPCDeserializer(functions: Func[]) {
 #include "midiRPCFunctions.h"
 #include "midiRPCDeserializer.h"
 #include "midiRPCReceiver.h"
+#include "arduino-debug-utils.h"
 
 namespace midiRPC {
     
   ${generateFunctionNamesCpp(functions)}
 
-  uint16_t deserializeAndCallFunction(uint8_t* data, uint16_t startPos) {
+  uint16_t deserializeAndCallFunction(const uint8_t* data, uint16_t startPos) {
     uint16_t pos = startPos;
-    uint16_t functionId = getFunctionId(data, pos+=2);
+    uint16_t functionId = getFunctionId(data, pos);
+    pos+=2;
 
 ${functions.map(functionMapper).join('')}
 
@@ -24,15 +26,25 @@ ${functions.map(functionMapper).join('')}
 function functionMapper(func: Func) {
     let paramsString = func.params.map(paramConverterMapper).join('\n')
     if(paramsString.length > 0) paramsString = `\n${paramsString}`
+
     return `
-    if(functionId == ${func.name}Id) {${paramsString}        
+    if(functionId == ${func.name}Id) {${paramsString}
+      ${getSerialPrintln(func)}        
       ${func.name}(${func.params.map((param) => param.name).join(', ')});
     }
 `
 }
 
+function getSerialPrintln(func: Func) {
+    if(func.params.length > 0){
+        return `serialPrintln(String("Calling ${func.name} with params ") + ${func.params.map((param) => param.name).join(' + ", " + ')});`
+    } else {
+        return `serialPrintln("Calling ${func.name}");`
+    }
+}
+
 function paramConverterMapper(param: Param) {
     const type = dataTypeMap[param.type]
-    return `      ${type.cppType} ${param.name} = ${type.deserializer}(data, pos+=${type.byteLength});`
+    return `      ${type.cppType} ${param.name} = ${type.deserializer}(data, pos);pos+=${type.byteLength};`
 }
 
