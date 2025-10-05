6eb75e79f1aeafa2e6243e520d43d9961fe1fc54
Joakim Tysseng <joakim.tysseng@bekk.no>
2025-10-05 20:47:44 +0200

    Working on colors and center part

--- src/components/modules/MainDisplay.tsx
+++ src/components/modules/MainDisplay.tsx
@@ -13,189 +13,199 @@ import './MainDisplay.scss'
 import mainDisplayControllers from '../../synthcore/modules/mainDisplay/mainDisplayControllers'
 import { dispatch } from '../../synthcore/utils'
 import { click, release } from '../../synthcore/modules/ui/uiReducer'
-
-interface Props {
-    x: number,
-    y: number
-}
+import { ModuleProps } from "./types";
+import { BORDER_MARGIN, POT_OFFSET_Y, ROW_HEIGHT } from "../../constants";
 
 const ctrlGroup = ControllerGroupIds.MAIN_DISP
 
 const SHIFT_KEYS = ['16', 'Shift'];
 
-const MainDisplay = React.forwardRef<SVGRectElement, Props>(({ x, y }, displayRef) => {
+const MainDisplay = React.forwardRef<SVGRectElement, ModuleProps>(
+    ({ x, y, height, width }, displayRef
+    ) => {
 
-    const currScreen = useAppSelector(selectCurrScreen)
+        const currScreen = useAppSelector(selectCurrScreen)
 
-    // approx. 9"
-    const displayWidth = 180
-    const displayHeight = 9 * displayWidth / 16
-    const displayCenter = x + displayWidth / 2
+        // approx. 9"
+        const displayWidth = 180
+        const displayHeight = 9 * displayWidth / 16
 
-    const bezelLRMargin = 20
-    const bezelTopMargin = 40
-    //const bezelBottomMargin = 60;
-    const bezelBottomMargin = 90
-
-    const buttonSpacing = 30
-    const buttonRow = y - 25
-
-    const potSpacing = 30
-    const potRow = y + displayHeight + 20
-    const masterPotRow = potRow + 30
-    const ctrlSwitchesRow1 = masterPotRow - 10
-    const ctrlSwitchesRow2 = masterPotRow + 10
-
-    // PC Keyboard handlers
-    const handleOnClick = useCallback(({ key }: {key: any}) => {
-        if (SHIFT_KEYS.includes(String(key))) {
-            dispatch(click({
-                ctrlGroup,
-                ctrl: mainDisplayControllers.FUNC_SHIFT,
-                source: ApiSource.UI
-            }))
-        }
-    }, [])
-
-    const handleOnRelease = useCallback(({ key }: {key: any}) => {
-        if (SHIFT_KEYS.includes(String(key))) {
-            dispatch(release({ ctrlGroup, ctrl: mainDisplayControllers.FUNC_SHIFT, source: ApiSource.UI }))
-        }
-    }, [])
-
-    useEventListener('keydown', handleOnClick);
-    useEventListener('keyup', handleOnRelease);
-
-    return <>
-        <rect x={x - bezelLRMargin} y={y - bezelTopMargin} height={displayHeight + bezelTopMargin + bezelBottomMargin} width={displayWidth + 2 * bezelLRMargin} className="bezel"/>
-        <RoundPushButton8 x={displayCenter - 2.5 * buttonSpacing} y={buttonRow}
-                          label="LFOs"
-                          labelPosition="bottom"
-                          ctrlGroup={ctrlGroup}
-                          radioButtonIndex={0}
-                          ctrl={mainDisplayControllers.GROUP_MENU}
-        />
-        <RoundPushButton8 x={displayCenter - 1.5 * buttonSpacing} y={buttonRow}
-                          label="Osc"
-                          labelPosition="bottom"
-                          ctrlGroup={ctrlGroup}
-                          radioButtonIndex={1}
-                          ctrl={mainDisplayControllers.GROUP_MENU}
-        />
-        <RoundPushButton8 x={displayCenter - 0.5 * buttonSpacing} y={buttonRow}
-                          label="Filters"
-                          labelPosition="bottom"
-                          ctrlGroup={ctrlGroup}
-                          radioButtonIndex={2}
-                          ctrl={mainDisplayControllers.GROUP_MENU}
-        />
-        <RoundPushButton8 x={displayCenter + 0.5 * buttonSpacing} y={buttonRow}
-                          label="Envelopes"
-                          labelPosition="bottom"
-                          ctrlGroup={ctrlGroup}
-                          radioButtonIndex={3}
-                          ctrl={mainDisplayControllers.GROUP_MENU}
-        />
-        <RoundPushButton8 x={displayCenter + 1.5 * buttonSpacing} y={buttonRow}
-                          label="Mods"
-                          labelPosition="bottom"
-                          ctrlGroup={ctrlGroup}
-                          radioButtonIndex={4}
-                          ctrl={mainDisplayControllers.GROUP_MENU}
-        />
-        <RoundPushButton8 x={displayCenter + 2.5 * buttonSpacing} y={buttonRow}
-                          label="FX"
-                          labelPosition="bottom"
-                          ctrlGroup={ctrlGroup}
-                          radioButtonIndex={5}
-                          ctrl={mainDisplayControllers.GROUP_MENU}
-        />
-        <Display x={x} y={y} width={displayWidth} height={displayHeight} ref={displayRef}/>
-
-        <RotaryPotWOLeds10 x={displayCenter - 2.5 * potSpacing} y={potRow}
-                           silver
-                           ctrlGroup={ControllerGroupIds.MAIN_DISP}
-                           ctrl={mainDisplayControllers.POT1}
-                           resolution={getPotResolution(mainDisplayControllers.POT1.id, currScreen)}
-        />
-        <RotaryPotWOLeds10 x={displayCenter - 1.5 * potSpacing} y={potRow}
-                           silver
-                           ctrlGroup={ControllerGroupIds.MAIN_DISP}
-                           ctrl={mainDisplayControllers.POT2}
-                           resolution={getPotResolution(mainDisplayControllers.POT2.id, currScreen)}
-        />
-        <RotaryPotWOLeds10 x={displayCenter - 0.5 * potSpacing} y={potRow}
-                           silver
-                           ctrlGroup={ControllerGroupIds.MAIN_DISP}
-                           ctrl={mainDisplayControllers.POT3}
-                           resolution={getPotResolution(mainDisplayControllers.POT3.id, currScreen)}
-        />
-        <RotaryPotWOLeds10 x={displayCenter + 0.5 * potSpacing} y={potRow}
-                           silver
-                           ctrlGroup={ControllerGroupIds.MAIN_DISP}
-                           ctrl={mainDisplayControllers.POT4}
-                           resolution={getPotResolution(mainDisplayControllers.POT4.id, currScreen)}
-        />
-        <RotaryPotWOLeds10 x={displayCenter + 1.5 * potSpacing} y={potRow}
-                           silver
-                           ctrlGroup={ControllerGroupIds.MAIN_DISP}
-                           ctrl={mainDisplayControllers.POT5}
-                           resolution={getPotResolution(mainDisplayControllers.POT5.id, currScreen)}
-        />
-        <RotaryPotWOLeds10 x={displayCenter + 2.5 * potSpacing} y={potRow}
-                           silver
-                           ctrlGroup={ControllerGroupIds.MAIN_DISP}
-                           ctrl={mainDisplayControllers.POT6}
-                           resolution={getPotResolution(mainDisplayControllers.POT6.id, currScreen)}
-        />
-        <RotaryPotWOLeds24 x={displayCenter} y={masterPotRow}
-                           ctrlGroup={ControllerGroupIds.MAIN_DISP}
-                           ctrl={mainDisplayControllers.POT7}
-                           resolution={getPotResolution(mainDisplayControllers.POT7.id, currScreen)}
-        />
-
-        <RoundPushButton8 x={displayCenter - 2.5 * buttonSpacing} y={ctrlSwitchesRow1} label="Home" labelPosition="bottom"
-                          ctrlGroup={ctrlGroup}
-                          ctrl={mainDisplayControllers.FUNC_HOME}
-        />
-
-        <RoundPushButton8 x={displayCenter - 1.5 * buttonSpacing} y={ctrlSwitchesRow1} label="Setup" labelPosition="bottom"
-                          ctrlGroup={ctrlGroup}
-                          ctrl={mainDisplayControllers.FUNC_SETTINGS}
-        />
-
-        <RoundPushButton8 x={displayCenter + 1.5 * buttonSpacing} y={ctrlSwitchesRow1} label="Load" labelPosition="bottom"
-                          ctrlGroup={ctrlGroup}
-                          ctrl={mainDisplayControllers.FUNC_LOAD}
-        />
-
-        <RoundPushButton8 x={displayCenter + 2.5 * buttonSpacing} y={ctrlSwitchesRow1} label="Save" labelPosition="bottom"
-                          ctrlGroup={ctrlGroup}
-                          ctrl={mainDisplayControllers.FUNC_SAVE}
-        />
+        const buttonRow = y + ROW_HEIGHT
+        const displayCenterX = x + width / 2
+        const displayCenterY = buttonRow + 2 * ROW_HEIGHT
 
 
-        <RoundPushButton8 x={displayCenter - 2.5 * buttonSpacing} y={ctrlSwitchesRow2} label="Shift" labelPosition="bottom"
-                          ctrlGroup={ctrlGroup}
-                          ctrl={mainDisplayControllers.FUNC_SHIFT}
-        />
+        const displayX = x + (width - displayWidth) / 2
+        const displayY = displayCenterY - displayHeight / 2
 
-        <RoundPushButton8 x={displayCenter - 1.5 * buttonSpacing} y={ctrlSwitchesRow2} label="Perform" labelPosition="bottom"
-                          ctrlGroup={ctrlGroup}
-                          ctrl={mainDisplayControllers.FUNC_PERFORM}
-        />
+        const buttonSpacing = 30
 
-        <RoundPushButton8 x={displayCenter + 1.5 * buttonSpacing} y={ctrlSwitchesRow2} label="Compare" labelPosition="bottom"
-                          ctrlGroup={ctrlGroup}
-                          ctrl={mainDisplayControllers.FUNC_COMPARE}
-        />
+        const potSpacing = 30
+        const potRow = y + ROW_HEIGHT * 5 + 2
+        const masterPotRow = y + ROW_HEIGHT * 6
+        const ctrlSwitchesRow1 = masterPotRow - 10
+        const ctrlSwitchesRow2 = masterPotRow + 10
 
-        <RoundPushButton8 x={displayCenter + 2.5 * buttonSpacing} y={ctrlSwitchesRow2} label="Route" labelPosition="bottom"
-                          ctrlGroup={ctrlGroup}
-                          ctrl={mainDisplayControllers.FUNC_ROUTE}
-        />
+        // PC Keyboard handlers
+        const handleOnClick = useCallback(({ key }: { key: any }) => {
+            if (SHIFT_KEYS.includes(String(key))) {
+                dispatch(click({
+                    ctrlGroup,
+                    ctrl: mainDisplayControllers.FUNC_SHIFT,
+                    source: ApiSource.UI
+                }))
+            }
+        }, [])
 
-    </>
-})
+        const handleOnRelease = useCallback(({ key }: { key: any }) => {
+            if (SHIFT_KEYS.includes(String(key))) {
+                dispatch(release({ ctrlGroup, ctrl: mainDisplayControllers.FUNC_SHIFT, source: ApiSource.UI }))
+            }
+        }, [])
+
+        useEventListener('keydown', handleOnClick);
+        useEventListener('keyup', handleOnRelease);
+
+        return <>
+            <rect x={x + BORDER_MARGIN} y={y + 22}
+                  height={height - BORDER_MARGIN- 75} width={width - 2 * BORDER_MARGIN}
+                  className="bezel"/>
+            <RoundPushButton8 x={displayCenterX - 2.5 * buttonSpacing} y={buttonRow}
+                              label="LFOs"
+                              labelPosition="bottom"
+                              ctrlGroup={ctrlGroup}
+                              radioButtonIndex={0}
+                              ctrl={mainDisplayControllers.GROUP_MENU}
+            />
+            <RoundPushButton8 x={displayCenterX - 1.5 * buttonSpacing} y={buttonRow}
+                              label="Osc"
+                              labelPosition="bottom"
+                              ctrlGroup={ctrlGroup}
+                              radioButtonIndex={1}
+                              ctrl={mainDisplayControllers.GROUP_MENU}
+            />
+            <RoundPushButton8 x={displayCenterX - 0.5 * buttonSpacing} y={buttonRow}
+                              label="Filters"
+                              labelPosition="bottom"
+                              ctrlGroup={ctrlGroup}
+                              radioButtonIndex={2}
+                              ctrl={mainDisplayControllers.GROUP_MENU}
+            />
+            <RoundPushButton8 x={displayCenterX + 0.5 * buttonSpacing} y={buttonRow}
+                              label="Envelopes"
+                              labelPosition="bottom"
+                              ctrlGroup={ctrlGroup}
+                              radioButtonIndex={3}
+                              ctrl={mainDisplayControllers.GROUP_MENU}
+            />
+            <RoundPushButton8 x={displayCenterX + 1.5 * buttonSpacing} y={buttonRow}
+                              label="Mods"
+                              labelPosition="bottom"
+                              ctrlGroup={ctrlGroup}
+                              radioButtonIndex={4}
+                              ctrl={mainDisplayControllers.GROUP_MENU}
+            />
+            <RoundPushButton8 x={displayCenterX + 2.5 * buttonSpacing} y={buttonRow}
+                              label="FX"
+                              labelPosition="bottom"
+                              ctrlGroup={ctrlGroup}
+                              radioButtonIndex={5}
+                              ctrl={mainDisplayControllers.GROUP_MENU}
+            />
+            <Display x={displayX} y={displayY} width={displayWidth} height={displayHeight} ref={displayRef}/>
+
+            <RotaryPotWOLeds10 x={displayCenterX - 2.5 * potSpacing} y={potRow}
+                               silver
+                               ctrlGroup={ControllerGroupIds.MAIN_DISP}
+                               ctrl={mainDisplayControllers.POT1}
+                               resolution={getPotResolution(mainDisplayControllers.POT1.id, currScreen)}
+            />
+            <RotaryPotWOLeds10 x={displayCenterX - 1.5 * potSpacing} y={potRow}
+                               silver
+                               ctrlGroup={ControllerGroupIds.MAIN_DISP}
+                               ctrl={mainDisplayControllers.POT2}
+                               resolution={getPotResolution(mainDisplayControllers.POT2.id, currScreen)}
+            />
+            <RotaryPotWOLeds10 x={displayCenterX - 0.5 * potSpacing} y={potRow}
+                               silver
+                               ctrlGroup={ControllerGroupIds.MAIN_DISP}
+                               ctrl={mainDisplayControllers.POT3}
+                               resolution={getPotResolution(mainDisplayControllers.POT3.id, currScreen)}
+            />
+            <RotaryPotWOLeds10 x={displayCenterX + 0.5 * potSpacing} y={potRow}
+                               silver
+                               ctrlGroup={ControllerGroupIds.MAIN_DISP}
+                               ctrl={mainDisplayControllers.POT4}
+                               resolution={getPotResolution(mainDisplayControllers.POT4.id, currScreen)}
+            />
+            <RotaryPotWOLeds10 x={displayCenterX + 1.5 * potSpacing} y={potRow}
+                               silver
+                               ctrlGroup={ControllerGroupIds.MAIN_DISP}
+                               ctrl={mainDisplayControllers.POT5}
+                               resolution={getPotResolution(mainDisplayControllers.POT5.id, currScreen)}
+            />
+            <RotaryPotWOLeds10 x={displayCenterX + 2.5 * potSpacing} y={potRow}
+                               silver
+                               ctrlGroup={ControllerGroupIds.MAIN_DISP}
+                               ctrl={mainDisplayControllers.POT6}
+                               resolution={getPotResolution(mainDisplayControllers.POT6.id, currScreen)}
+            />
+            <RotaryPotWOLeds24 x={displayCenterX} y={masterPotRow}
+                               ctrlGroup={ControllerGroupIds.MAIN_DISP}
+                               ctrl={mainDisplayControllers.POT7}
+                               resolution={getPotResolution(mainDisplayControllers.POT7.id, currScreen)}
+            />
+
+            <RoundPushButton8 x={displayCenterX - 2.5 * buttonSpacing} y={ctrlSwitchesRow1} label="Home"
+                              labelPosition="bottom"
+                              ctrlGroup={ctrlGroup}
+                              ctrl={mainDisplayControllers.FUNC_HOME}
+            />
+
+            <RoundPushButton8 x={displayCenterX - 1.5 * buttonSpacing} y={ctrlSwitchesRow1} label="Setup"
+                              labelPosition="bottom"
+                              ctrlGroup={ctrlGroup}
+                              ctrl={mainDisplayControllers.FUNC_SETTINGS}
+            />
+
+            <RoundPushButton8 x={displayCenterX + 1.5 * buttonSpacing} y={ctrlSwitchesRow1} label="Load"
+                              labelPosition="bottom"
+                              ctrlGroup={ctrlGroup}
+                              ctrl={mainDisplayControllers.FUNC_LOAD}
+            />
+
+            <RoundPushButton8 x={displayCenterX + 2.5 * buttonSpacing} y={ctrlSwitchesRow1} label="Save"
+                              labelPosition="bottom"
+                              ctrlGroup={ctrlGroup}
+                              ctrl={mainDisplayControllers.FUNC_SAVE}
+            />
+
+
+            <RoundPushButton8 x={displayCenterX - 2.5 * buttonSpacing} y={ctrlSwitchesRow2} label="Shift"
+                              labelPosition="bottom"
+                              ctrlGroup={ctrlGroup}
+                              ctrl={mainDisplayControllers.FUNC_SHIFT}
+            />
+
+            <RoundPushButton8 x={displayCenterX - 1.5 * buttonSpacing} y={ctrlSwitchesRow2} label="Perform"
+                              labelPosition="bottom"
+                              ctrlGroup={ctrlGroup}
+                              ctrl={mainDisplayControllers.FUNC_PERFORM}
+            />
+
+            <RoundPushButton8 x={displayCenterX + 1.5 * buttonSpacing} y={ctrlSwitchesRow2} label="Compare"
+                              labelPosition="bottom"
+                              ctrlGroup={ctrlGroup}
+                              ctrl={mainDisplayControllers.FUNC_COMPARE}
+            />
+
+            <RoundPushButton8 x={displayCenterX + 2.5 * buttonSpacing} y={ctrlSwitchesRow2} label="Route"
+                              labelPosition="bottom"
+                              ctrlGroup={ctrlGroup}
+                              ctrl={mainDisplayControllers.FUNC_ROUTE}
+            />
+
+        </>
+    })
 
 export default MainDisplay
--- src/components/modules/VoiceSelector.tsx
+++ src/components/modules/VoiceSelector.tsx
@@ -2,70 +2,67 @@ import React from 'react';
 import RoundLedPushButton8 from '../buttons/RoundLedPushButton8';
 import { ControllerGroupIds } from '../../synthcore/types'
 import voicesControllers from '../../synthcore/modules/voices/voicesControllers'
-
-interface Props {
-    x: number,
-    y: number
-}
+import { ModuleProps } from "./types";
 
 const ctrlGroup = ControllerGroupIds.VOICES
 
-const VoiceSelector = ({ x, y }: Props) => {
-    const offsetX = 5;
+const VoiceSelector = ({ x, y, width, height }: ModuleProps) => {
+    const buttonRow = y
     const buttonDistance = 25;
+    const offsetX = x + (width - 7 * buttonDistance) / 2
 
     //const voices = useAppSelector(selectController(voicesControllers.VOICE))
 
-    return <svg x={x} y={y}>
-        <RoundLedPushButton8 labelPosition="bottom" x={offsetX} y={0} label="1"
+    return <>
+        <RoundLedPushButton8 labelPosition="bottom" x={offsetX} y={buttonRow} label="1"
                              ctrlGroup={ctrlGroup}
                              ctrl={voicesControllers.VOICE}
                              radioButtonIndex={0}
         />
 
-        <RoundLedPushButton8 labelPosition="bottom" x={offsetX + buttonDistance} y={0} label="2"
+        <RoundLedPushButton8 labelPosition="bottom" x={offsetX + buttonDistance} y={buttonRow} label="2"
                              ctrlGroup={ctrlGroup}
                              ctrl={voicesControllers.VOICE}
                              radioButtonIndex={1}
         />
 
-        <RoundLedPushButton8 labelPosition="bottom" x={offsetX + buttonDistance * 2} y={0} label="3"
+        <RoundLedPushButton8 labelPosition="bottom" x={offsetX + buttonDistance * 2} y={buttonRow} label="3"
                              ctrlGroup={ctrlGroup}
                              ctrl={voicesControllers.VOICE}
                              radioButtonIndex={2}
         />
 
-        <RoundLedPushButton8 labelPosition="bottom" x={offsetX + buttonDistance * 3} y={0} label="4"
+        <RoundLedPushButton8 labelPosition="bottom" x={offsetX + buttonDistance * 3} y={buttonRow} label="4"
                              ctrlGroup={ctrlGroup}
                              ctrl={voicesControllers.VOICE}
                              radioButtonIndex={3}
         />
 
-        <RoundLedPushButton8 labelPosition="bottom" x={offsetX + buttonDistance * 4} y={0} label="5"
+        <RoundLedPushButton8 labelPosition="bottom" x={offsetX + buttonDistance * 4} y={buttonRow} label="5"
                              ctrlGroup={ctrlGroup}
                              ctrl={voicesControllers.VOICE}
                              radioButtonIndex={4}
         />
 
-        <RoundLedPushButton8 labelPosition="bottom" x={offsetX + buttonDistance * 5} y={0} label="6"
+        <RoundLedPushButton8 labelPosition="bottom" x={offsetX + buttonDistance * 5} y={buttonRow} label="6"
                              ctrlGroup={ctrlGroup}
                              ctrl={voicesControllers.VOICE}
                              radioButtonIndex={5}
         />
 
-        <RoundLedPushButton8 labelPosition="bottom" x={offsetX + buttonDistance * 6} y={0} label="7"
+        <RoundLedPushButton8 labelPosition="bottom" x={offsetX + buttonDistance * 6} y={buttonRow} label="7"
                              ctrlGroup={ctrlGroup}
                              ctrl={voicesControllers.VOICE}
                              radioButtonIndex={6}
         />
 
-        <RoundLedPushButton8 labelPosition="bottom" x={offsetX + buttonDistance * 7} y={0} label="8"
+        <RoundLedPushButton8 labelPosition="bottom" x={offsetX + buttonDistance * 7} y={buttonRow} label="8"
                              ctrlGroup={ctrlGroup}
                              ctrl={voicesControllers.VOICE}
                              radioButtonIndex={7}
         />
 
-    </svg>;
+    </>;
 };
 
 export default VoiceSelector;
