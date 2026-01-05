7535909564f1ed0187c0d2f35ef39773106ec704
Joakim Tysseng <joakim.tysseng@bekk.no>
2026-01-05 21:02:59 +0100

    Removed StrictMode to prevent react from calling stuff twice in development

--- src/index.tsx
+++ src/index.tsx
@@ -10,12 +10,10 @@ import midiApi from './midi/midiApi'
 midiApi.initReceive()
 
 ReactDOM.render(
-  <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
