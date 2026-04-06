7118c24566f17cc5694f1aaac814398e46ea3415
Joakim Tysseng <joakim.tysseng@bekk.no>
2026-04-06 17:37:01 +0200

    Fixed bug on mod screen

--- src/controller/mods/ModControl.tsx
+++ src/controller/mods/ModControl.tsx
@@ -9,28 +9,30 @@ const ModControl = () => {
     return (
         <div className="mod-ctrl">
             <ScrollSync>
-                <div className="mod-ctrl__header">
-                    <div className="mod-ctrl__header__corner">
-                        <div className="mod-ctrl__header__corner__top"></div>
-                        <div className="mod-ctrl__header__corner__bottom">Mod source</div>
+                <div className="mod-ctrl__inner">
+                    <div className="mod-ctrl__header">
+                        <div className="mod-ctrl__header__corner">
+                            <div className="mod-ctrl__header__corner__top"></div>
+                            <div className="mod-ctrl__header__corner__bottom">Mod source</div>
+                        </div>
+                        <ScrollSyncNode lockAxis="Y">
+                            <div className="mod-ctrl__header__dsts-container" id="dsts">
+                                <DstLabels />
+                            </div>
+                        </ScrollSyncNode>
                     </div>
-                    <ScrollSyncNode lockAxis="Y">
-                        <div className="mod-ctrl__header__dsts-container" id="dsts">
-                            <DstLabels />
-                        </div>
-                    </ScrollSyncNode>
-                </div>
-                <div className="mod-ctrl__content">
-                    <ScrollSyncNode lockAxis="X">
-                        <div className="mod-ctrl__content__sources-container" id="sources">
-                            <SourceLabels />
-                        </div>
-                    </ScrollSyncNode>
-                    <ScrollSyncNode>
-                        <div className="mod-ctrl__content__amounts-container" id="amounts">
-                            <AmountsTable />
-                        </div>
-                    </ScrollSyncNode>
+                    <div className="mod-ctrl__content">
+                        <ScrollSyncNode lockAxis="X">
+                            <div className="mod-ctrl__content__sources-container" id="sources">
+                                <SourceLabels />
+                            </div>
+                        </ScrollSyncNode>
+                        <ScrollSyncNode>
+                            <div className="mod-ctrl__content__amounts-container" id="amounts">
+                                <AmountsTable />
+                            </div>
+                        </ScrollSyncNode>
+                    </div>
                 </div>
             </ScrollSync>
         </div>
